e2dac6bf273b70c6aeffee8e1b8330a2d5dade67
Rohan <83433649+RohanSen08@users.noreply.github.com>
2025-10-04 17:34:39 -0400

    rewrote beam break

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/BeamBreaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/BeamBreaker.java
@@ -10,29 +10,22 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @Config
-@Autonomous(name = "BeakBreakTesting", group = "123123123")
+@Autonomous(name = "BeamBreakTesting", group = "123123123")
 public class BeamBreaker extends LinearOpMode {
-    HardwareMap hardwareMap;
-
-    DigitalChannel beamBreak;
-
-    public BeamBreaker(HardwareMap hardwareMap) {
-        this.hardwareMap = hardwareMap;
-    }
 
     @Override
     public void runOpMode() {
-        beamBreak = hardwareMap.get(DigitalChannel.class, "beam");
+        DigitalChannel beamBreak;
+        DigitalChannel beamBreak2;
+        beamBreak = hardwareMap.get(DigitalChannel.class, "beam1");
         beamBreak.setMode(DigitalChannel.Mode.INPUT);
-        TelemetryPacket telemetryPacket = new TelemetryPacket();
+        beamBreak.setState(true);
+        beamBreak2 = hardwareMap.get(DigitalChannel.class, "beam2");
+        beamBreak2.setMode(DigitalChannel.Mode.OUTPUT);
         waitForStart();
 
         while (opModeIsActive()) {
-            if (beamBreak.getState()) {
-                telemetryPacket.put("Beam", "Unbroken");
-            } else {
-                telemetryPacket.put("Beam", "Broken");
-            }
+            telemetry.addData("Beam", beamBreak.getState());
             telemetry.update();
         }
     }

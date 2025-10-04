4f3802e219301ed70e7c1473158b202c8f413456
Rohan <83433649+RohanSen08@users.noreply.github.com>
2025-10-04 10:05:36 -0400

    i think this op works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tuners/TuneAccel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tuners/TuneAccel.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpModes.Tuners;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Hardware.Sensors.Battery;
 import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Drivetrain;
@@ -9,25 +10,36 @@ public class TuneAccel extends LinearOpMode {
     @Override
     public void runOpMode() {
         double stopPos = 0;
+        double presentPos = 0;
+        double lastPos;
         boolean isStopped = false;
         Battery battery = new Battery(hardwareMap);
         Drivetrain drivetrain = new Drivetrain(hardwareMap, battery);
+        ElapsedTime timer = new ElapsedTime();
         waitForStart();
         while (opModeIsActive()) {
-            if (gamepad1.a) {
+            if (gamepad1.a && !isStopped) { //Using isStopped as a total cut to controls
                 drivetrain.motorLeftFront.setPower(1);
                 drivetrain.motorRightFront.setPower(1);
                 drivetrain.motorRightBack.setPower(1);
                 drivetrain.motorLeftBack.setPower(1);
-            } else if (gamepad1.b) {
+            } else if (gamepad1.b && !isStopped) {
                 drivetrain.motorLeftFront.setPower(0);
                 drivetrain.motorRightFront.setPower(0);
                 drivetrain.motorRightBack.setPower(0);
                 drivetrain.motorLeftBack.setPower(0);
                 isStopped = true;
+                timer.reset();
                 stopPos = drivetrain.state.get(0, 0);
+                presentPos = stopPos;
             } else if (isStopped) {
-
+                lastPos = presentPos;
+                presentPos = drivetrain.state.get(0, 0);
+                telemetry.addData("Stopping distance: ", stopPos - presentPos);
+                telemetry.addData("Stopping time: ", timer.seconds());
+                if (Math.abs((lastPos - presentPos)) < 0.01) {
+                    isStopped = false;
+                }
             }
         }
     }

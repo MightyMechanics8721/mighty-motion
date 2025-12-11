d14bb1bd748e8e5e984631af85843b7adbb63f21
Alexander Koldy <alexanderkoldy.ak@gmail.com>
2025-12-11 16:24:00 -0500

    kevin on my laptop.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Shooter/Shooter.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.teamcode.Mechanisms.Utils.Controllers.Constants.FFC
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.InstantAction;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Mechanisms.Utils.Controllers.Constants.PIDConstants;
@@ -24,7 +25,8 @@ public class Shooter {
     /**
      * Configuration parameters for the motor controller (PID and FF constants).
      */
-    public static MotorControllerConstants MOTOR_CONTROLLER_CONSTANTS = new MotorControllerConstants();
+    public static MotorControllerConstants MOTOR_CONTROLLER_CONSTANTS
+            = new MotorControllerConstants();
 
     /**
      * Configuration names for hardware mapping.
@@ -38,17 +40,21 @@ public class Shooter {
      * Constructs a new Shooter mechanism and initializes its motor controller.
      *
      * @param hardwareMap the FTC HardwareMap used to retrieve motor hardware
-     * @param battery     the Battery instance used to monitor voltage and apply compensation
+     * @param battery the Battery instance used to monitor voltage and apply compensation
      */
     public Shooter(HardwareMap hardwareMap, Battery battery) {
 
         this.motorController = new MotorController(
                 hardwareMap,
-                new String[]{CONFIGURATION_NAMES.shooterMotor1Name, CONFIGURATION_NAMES.shooterMotor2Name},
+                new String[]{
+                        CONFIGURATION_NAMES.shooterMotor1Name,
+                        CONFIGURATION_NAMES.shooterMotor2Name
+                },
                 battery,
                 BATTERY_PARAMETERS.maxVoltage,
                 CONFIGURATION_NAMES.encoderName,
-                28.0);
+                28.0
+        );
 
         this.motorController.setVelocityPIDConstants(MOTOR_CONTROLLER_CONSTANTS.pidConstants);
         this.motorController.setVelocityFeedForwardConstants(MOTOR_CONTROLLER_CONSTANTS.ffConstants);
@@ -68,22 +74,29 @@ public class Shooter {
      *
      * @param velocity the target velocity to set for the shooter motors
      */
-//    public void setVelocity(double velocity) {
-//        this.motorController.setVelocity(velocity);
-//    }
+    //    public void setVelocity(double velocity) {
+    //        this.motorController.setVelocity(velocity);
+    //    }
     public Action setShooterVelocity(double velocity) {
         return new Action() {
             @Override
             public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
                 Shooter.this.motorController.setVelocity(velocity);
+
                 return false;
             }
         };
     }
 
+    public InstantAction revShooter(double velocity) {
+
+        return new InstantAction(this.motorController.setVelocity(velocity));
+    }
+
     /**
-     * Holds configuration names for the shooter hardware.
-     * These correspond to names in the robot configuration file.
+     * Holds configuration names for the shooter hardware. These correspond to names in the robot
+     * configuration file.
      */
     public static class ConfigurationNames {
 
@@ -104,8 +117,8 @@ public class Shooter {
     }
 
     /**
-     * Contains configuration parameters related to the battery.
-     * These parameters are used to account for voltage variations.
+     * Contains configuration parameters related to the battery. These parameters are used to
+     * account for voltage variations.
      */
     public static class BatteryParameters {
 
@@ -116,8 +129,8 @@ public class Shooter {
     }
 
     /**
-     * Holds PID and feedforward constants used by the motor controller.
-     * These constants can be tuned via the FTC Dashboard.
+     * Holds PID and feedforward constants used by the motor controller. These constants can be
+     * tuned via the FTC Dashboard.
      */
     public static class MotorControllerConstants {
 

3b4e51198a87915bc8febde30f86e4ecf41812ae
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-05-28 19:34:00 -0400

    limelight map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Limelight/Tuners/TestLimelightPose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Limelight/Tuners/TestLimelightPose.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Mechanisms.Limelight.Tuners;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.canvas.Canvas;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.hardware.limelightvision.LLResult;
@@ -8,7 +9,12 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.ejml.simple.SimpleMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Drawing;
+import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Utils.Utils;
+import org.firstinspires.ftc.teamcode.Mechanisms.Utils.Controllers.PID;
 
 @Config
 @TeleOp(name = "Tune LL", group = "123Competition")
@@ -21,7 +27,6 @@ public class TestLimelightPose extends LinearOpMode {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
         FtcDashboard dashboard = FtcDashboard.getInstance();
-        telemetry = dashboard.getTelemetry();
 
         limelight.pipelineSwitch(1);
         waitForStart();
@@ -30,15 +35,25 @@ public class TestLimelightPose extends LinearOpMode {
          */
         limelight.start();
         while (opModeIsActive()) {
+            TelemetryPacket packet = new TelemetryPacket();
             LLResult result = limelight.getLatestResult();
             if (result != null) {
                 if (result.isValid()) {
                     Pose3D botpose = result.getBotpose();
-                    telemetry.addData("x", botpose.getPosition().x * 39.37);
-                    telemetry.addData("y", botpose.getPosition().y * 39.37);
+                    packet.put("x", botpose.getPosition().x * 39.37);
+                    packet.put("y", botpose.getPosition().y * 39.37);
+                    packet.put("theta", botpose.getOrientation().getYaw(AngleUnit.DEGREES));
+                    SimpleMatrix botState = Utils.makePoseVector(
+                            botpose.getPosition().x * 39.37,
+                            botpose.getPosition().y * 39.37,
+                            botpose.getOrientation().getYaw(AngleUnit.DEGREES)
+                    );
+
+                    Canvas canvas = packet.fieldOverlay();
+                    Drawing.drawRobot(botState, canvas, "black");
+                    dashboard.sendTelemetryPacket(packet);
                 }
             }
-            telemetry.update();
         }
     }
 }

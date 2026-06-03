a9792599c08ec98ffa057802bd096c2f62dd2c56
Rohan <83433649+RohanSen08@users.noreply.github.com>
2026-06-03 19:50:03 -0400

    noise

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Limelight/Tuners/PinpointDrift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Limelight/Tuners/PinpointDrift.java
@@ -9,6 +9,7 @@ import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.ejml.simple.SimpleMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -25,8 +26,8 @@ import org.firstinspires.ftc.teamcode.Mechanisms.Turret.Turret;
 import org.firstinspires.ftc.teamcode.Mechanisms.Utils.Controllers.PID;
 
 @Config
-@TeleOp(name = "Tune LL", group = "123Competition")
-public class TestLimelightPose extends LinearOpMode {
+@TeleOp(name = "PP Drift", group = "123Competition")
+public class PinpointDrift extends LinearOpMode {
 
     private Limelight3A limelight;
     private Drivetrain drivetrain;
@@ -34,7 +35,6 @@ public class TestLimelightPose extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
         Battery.initialize(hardwareMap);
         Shooter.initialize(hardwareMap);
         Drivetrain.initialize(hardwareMap);
@@ -42,41 +42,48 @@ public class TestLimelightPose extends LinearOpMode {
         shooter = Shooter.getInstance();
 
         FtcDashboard dashboard = FtcDashboard.getInstance();
-
-        limelight.pipelineSwitch(1);
+        ElapsedTime timer = new ElapsedTime();
+        double[] xPos = new double[100];
+        double[] yPos = new double[100];
+        double[] tPos = new double[100];
+        int loops = 0;
+        int seconds = 0;
+        double xSum = 0;
+        double ySum = 0;
+        double tSum = 0;
         waitForStart();
-        /*
-         * Starts polling for data.
-         */
-        limelight.start();
+        timer.reset();
         while (opModeIsActive()) {
             TelemetryPacket packet = new TelemetryPacket();
-            LLResult result = limelight.getLatestResult();
-            Actions.runBlocking(shooter.setShooterVelocityLoop(1500));
             drivetrain.localize();
-            if (result != null) {
-                if (result.isValid()) {
-                    Pose3D botpose = result.getBotpose();
-                    packet.put("x_ll", botpose.getPosition().x * 39.37);
-                    packet.put("y_ll", botpose.getPosition().y * 39.37);
-                    packet.put("theta_ll", botpose.getOrientation().getYaw(AngleUnit.DEGREES));
-                    packet.put("x_pp", drivetrain.state.get(0));
-                    packet.put("y_pp", drivetrain.state.get(1));
-                    packet.put("theta_pp", drivetrain.state.get(2));
-                    packet.put("stdev x", result.getStddevMt1()[0] * 39.37);
-                    packet.put("stdev y", result.getStddevMt1()[1] * 39.37);
-                    packet.put("stdev theta", result.getStddevMt1()[5]);
-                    SimpleMatrix botState = Utils.makePoseVector(
-                            botpose.getPosition().x * 39.37,
-                            botpose.getPosition().y * 39.37,
-                            botpose.getOrientation().getYaw(AngleUnit.DEGREES)
-                    );
-
-                    Canvas canvas = packet.fieldOverlay();
-                    Drawing.drawRobot(botState, canvas, "black");
-                    dashboard.sendTelemetryPacket(packet);
+            Actions.runBlocking(shooter.setShooterVelocityLoop(1500));
+            packet.put("Heading", Math.toDegrees(drivetrain.state.get(2)));
+            if (seconds < 100) {
+                if (timer.seconds() < 1) {
+                    xSum += drivetrain.state.get(0);
+                    ySum += drivetrain.state.get(1);
+                    tSum += drivetrain.state.get(2);
+                    loops++;
+                } else {
+                    xPos[seconds] = xSum / loops;
+                    yPos[seconds] = ySum / loops;
+                    tPos[seconds] = tSum / loops;
+                    xSum = 0;
+                    ySum = 0;
+                    tSum = 0;
+                    loops = 0;
+                    timer.reset();
+                    seconds++;
                 }
+            } else {
+                packet.put("xDrift 0", xPos[0]);
+                packet.put("xDrift 99", xPos[99]);
+                packet.put("yDrift 0", yPos[0]);
+                packet.put("yDrift 99", yPos[99]);
+                packet.put("tDrift 0", Math.toDegrees(tPos[0]));
+                packet.put("tDrift 99", Math.toDegrees(tPos[99]));
             }
+            dashboard.sendTelemetryPacket(packet);
         }
     }
 }
